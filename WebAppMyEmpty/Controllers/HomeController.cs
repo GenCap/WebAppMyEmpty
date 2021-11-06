e9835ec9f822bea39811f63d0e29d1c7144ced23
Åsa Jonsson <asaj71@gmail.com>
2021-11-06 23:15:51 +0100

    add css styles

--- WebAppMyEmpty/Controllers/HomeController.cs
+++ WebAppMyEmpty/Controllers/HomeController.cs
@@ -12,5 +12,17 @@ namespace WebAppMyEmpty.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
     }
 }
